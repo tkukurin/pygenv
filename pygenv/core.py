b1ef597fade49c3944fb1939d8fbf9bb0b4458ce
Toni Kukurin <toni.kukurin@quantco.com>
2024-12-07 15:32:08 +0000

    trying out nbdev adding requirements, add typer as dependency

--- pygenv/core.py
+++ pygenv/core.py
@@ -7,6 +7,7 @@ __all__ = ['info', 'tell', 'ProcResult', 'sprun', 'GlobalEnv', 'env_make_activat
 
 # %% ../nbs/00_core.ipynb 3
 from functools import partial
+import json
 import os
 from pprint import pformat
 import sys
@@ -15,6 +16,7 @@ import subprocess as sproc
 
 import subprocess
 from dataclasses import dataclass, field
+from rich import print
 
 
 # informational vs output data
@@ -29,16 +31,22 @@ class ProcResult:
     ok: bool
     out: str
     err: str
-    raw: sproc.CompletedProcess = field(repr=False)
+    raw: None | sproc.CompletedProcess = field(repr=False, default=None)
 
 
-def sprun(*args, **kwargs) -> ProcResult:
-    result = subprocess.run(
-        map(str, args),
-        capture_output=True,
-        text=False,  # Keep raw output as bytes
-        **kwargs
-    )
+def sprun(*args, guard: bool = True, **kwargs) -> ProcResult:
+    try:
+        result = subprocess.run(
+            tuple(map(str, args)),
+            capture_output=True,
+            text=False,  # Keep raw output as bytes
+            **kwargs
+        )
+    except Exception as e:
+        if not guard:
+            raise
+        info(f"{args} failed: {e}")
+        return ProcResult(False, "", "", None)
     return ProcResult(
         ok=result.returncode == 0,
         out=result.stdout.decode('utf-8', errors='replace').strip() if result.stdout else "",
@@ -50,7 +58,7 @@ def sprun(*args, **kwargs) -> ProcResult:
 class GlobalEnv:
     """instance of a globally managed environment
     """
-
+    # vscode suggestion: {"python.venvFolders": ["~/tk/uv/"]}
     BASE = Path("~/tk/uv").expanduser() 
 
     def __init__(self, name: str):
@@ -69,46 +77,52 @@ class GlobalEnv:
                 break  # fix for "fishlogin"
         return self.path / "bin" / activate_file
     
-    def validate(env):
+    def validate(env, shell = None):
         feats = dict(
             has_root = env.path.exists(),
-            has_activate = env.activate_path.exists(),
             has_py = (env.path / "bin" / "python").exists(),
+            has_activate = env.activate_path(shell).exists(),
+            activate = env.activate_path(shell),
             py = sprun(
                 str(env.path / "bin" / "python"),
                 "--version"
             ).out,
+            detail = dict(
+                libs=json.loads(sprun("pip", "list", "--format=json").out or "[]"),
+            )
         )
         is_valid = all(v for k, v in feats.items() if k.startswith("has"))
         return is_valid, feats
 
 
-def env_make_activate(name: str):
+import typer
+_OPT_VERBOSE = typer.Option(
+    0, "--verbose", "-v", count=True, envvar="TK_DEBUG", max=1)
+
+def env_make_activate(
+    name: str,
+    verbose: int = _OPT_VERBOSE):
     outs = None
     env = GlobalEnv(name)
 
-    if env.exists():
-        info(f"Found {env.feats()=}")
-    else:
-        created = sprun("uv", "venv", env.path)
-        info(f"{created.ok=}")
-
     valid, feats = env.validate()
     if valid:
-        outs = f"{env.activate_path}"
-
-    info(f"Env feats: {pformat(feats)}")
+        outs = feats["activate"]
+    elif not feats["has_root"]:
+        info(f"Nothing found on {env.path}. Creating...")
+        created = sprun("uv", "venv", env.path)
+        info(f"{created.ok=}")
+    
+    if not verbose:
+        feats.pop("detail", None)
+    info(f"Env feats:\n{pformat(feats)}")
     return env, outs
 
 
 def cli():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("name", help="Environment name")
-    args = parser.parse_args()
-    env, outs = env_make_activate(args.name)
+    import typer
+    _, outs = typer.run(env_make_activate)
     tell(outs)
-    return env
 
 # cli()
 # CompletedProcess(args=['uv', 'venv', '/Users/toni/tk/uv/pygen'], returncode=0, stdout=b'', stderr=b'Using Python 3.11.6 interpreter at: \x1b[36m/Users/toni/miniconda3/envs/research/bin/python3.11\x1b[39m\nCreating virtualenv at: \x1b[36m/Users/toni/tk/uv/pygen\x1b[39m\n')
